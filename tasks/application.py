796b6ccc92104ab827b8bd266c4aa5f10cf38a5c
Andrew Dobbs <andrew.dobbs@gmail.com>
2020-05-17 09:31:35 -0400

    Create tasks app

---
+++ tasks/application.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def tasks():
+    return render_template("tasks.html")
+
+@app.route("/add")
+def add():
+    return render_template("add.html")
