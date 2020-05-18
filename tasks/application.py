2044da349be8371898976109cff7e3598c83207c
Andrew Dobbs <andrew.dobbs@gmail.com>
2020-05-18 13:20:31 -0400

    Import session

--- tasks/application.py
+++ tasks/application.py
@@ -1,6 +1,9 @@
-from flask import Flask, redirect, render_template, request
+from flask import Flask, redirect, render_template, request, session
+from flask_session import Session
 
 app = Flask(__name__)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
 
 todos = []
 
